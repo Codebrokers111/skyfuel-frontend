eea3c16229dfefc54a3035bca8579edf8376c73b
Mohd Zain <zainmohd1998@gmail.com>
2025-09-13 20:36:08 +0530

    chore: folder structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import HomeProd from "./home/HomeProd";
+import Footer from "./home/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <HomeProd />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -1,8 +1,8 @@
 import { useState, useContext, useEffect, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useGoogleLogin } from "@react-oauth/google";
-import { userContext } from "../context/userContext";
-import Loader from "./Loader";
+import { userContext } from "../../context/userContext";
+import Loader from "../../components/Loader";
 
 import ReCAPTCHA from "react-google-recaptcha";
 
@@ -66,8 +66,8 @@ const Signup = (props) => {
 
   if (loading) {
     return (
-      <div className="d-flex justify-content-center align-item-center my-2">
-        <span className="spinner-border"></span>
+      <div className="flex justify-center items-center my-2">
+        <Loader color="gray" size="large" />
       </div>
     );
   }
@@ -574,13 +574,6 @@ const Signup = (props) => {
               {/* end modal */}
             </div>
           </div>
-          {/* <button
-            type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            onClick={checkModal}
-          >
-            Default
-          </button> */}
         </div>
 
         {/* Right / Info side */}
